da339d94a09fc5eb1ec809317cfeca3c2d7a75aa
mohuishou <1@lailin.xyz>
2016-07-25 21:28:28 +0800

    【迁移】首页迁移完成

--- Application/Common/Common/developer.php
+++ Application/Common/Common/developer.php
@@ -8,3 +8,14 @@
 // +----------------------------------------------------------------------
 
 //开发者二次开发公共函数统一写入此文件，不要修改function.php以便于系统升级。
+
+/**
+ * 获取一级目录，包括图集相册，新闻、留言等模块
+ * @author mohuishou<1@lailin.xyz>
+ */
+function getCategory(){
+    $album_cat=D('Album/Category')->getCategoryTree();
+    $cms_cat=D('Cms/Category')->getCategoryTree();
+    $categories=array_merge($album_cat,$cms_cat);
+    return $categories;
+}
